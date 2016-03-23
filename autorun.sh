174ed067897bd9eecb5a5214d4d4af1a8589e29f
rvrv <lukasgrand@gmail.com>
2016-03-23 21:19:25 +0100

    Update autorun.sh

--- autorun.sh
+++ autorun.sh
@@ -8,4 +8,11 @@ echo "/usr/bin/killall mpd;exit 0" >> /etc/rcK.d/QK98mpd.sh
 /bin/mkdir /share/homes
 /bin/chown admin:administrator /share/homes
 /bin/ln -s /share/CE_CACHEDEV4_DATA/Homes/user /share/homes/user
+/bin/ln -s /share/CE_CACHEDEV4_DATA/Git /share/homes/git
+/bin/ln -s /opt/bin/git-upload-pack /usr/bin/git-upload-pack
+/bin/ln -s /opt/bin/git /usr/bin/git
+/bin/ln -s /opt/bin/git-cvsserver /usr/bin/git-cvsserver
+/bin/ln -s /opt/bin/git-shell /usr/bin/git-shell
+/bin/ln -s /opt/bin/git-receive-pack /usr/bin/git-receive-pack
+/bin/ln -s /opt/bin/git-upload-archive /usr/bin/git-upload-archive
 /opt/bin/mpd
